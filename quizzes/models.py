59439f444d395b0e02da546d47f5f0780ed1b0c9
nick <nicktran6997@gmail.com>
2020-08-07 15:17:25 -0700

    char length

--- quizzes/models.py
+++ quizzes/models.py
@@ -10,7 +10,7 @@ class Quiz(models.Model):
 
 class QuestionBlock(models.Model):
     #Every QuestionBlock has a question, an answer, and a quiz it belongs to
-    question = models.CharField(max_length=60)
+    question = models.CharField(max_length=40)
     correctChoice = models.IntegerField(default=0)
     quiz = models.ForeignKey(Quiz, related_name="questions", to_field="title", on_delete=models.CASCADE) #assume for this proj, we dont reuse same question for diff quizzes
 
@@ -22,7 +22,7 @@ class Choice(models.Model):
     #could also try serialization too...
 
     #Every Choice has some text and belongs to a questionBlock
-    text = models.CharField(max_length=100)
+    text = models.CharField(max_length=40)
     question = models.ForeignKey(QuestionBlock, related_name="choices", on_delete=models.CASCADE)
 
     def __str__(self):
