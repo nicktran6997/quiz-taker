c89ff4862703fb09633a08e088f430aa3c45b5bf
nick <nicktran6997@gmail.com>
2020-08-07 18:01:09 -0700

    maybe fixed postgres problem

--- quizzes/models.py
+++ quizzes/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 class Quiz(models.Model):
     #Every Quiz has a title
-    title = models.CharField(max_length=40, unique=True, primary_key=True)
+    title = models.CharField(max_length=100, unique=True, primary_key=True)
 
     def __str__(self):
         return self.title
 
 class QuestionBlock(models.Model):
     #Every QuestionBlock has a question, an answer, and a quiz it belongs to
-    question = models.CharField(max_length=40)
+    question = models.CharField(max_length=100)
     correctChoice = models.IntegerField(default=0)
     quiz = models.ForeignKey(Quiz, related_name="questions", to_field="title", on_delete=models.CASCADE) #assume for this proj, we dont reuse same question for diff quizzes
 
@@ -22,7 +22,7 @@ class Choice(models.Model):
     #could also try serialization too...
 
     #Every Choice has some text and belongs to a questionBlock
-    text = models.CharField(max_length=40)
+    text = models.CharField(max_length=100)
     question = models.ForeignKey(QuestionBlock, related_name="choices", on_delete=models.CASCADE)
 
     def __str__(self):
